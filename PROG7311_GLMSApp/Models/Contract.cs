ecdf6a8d6ca3b5a8e357b4765e28a513e239bc5c
diya singh <161337050+ST10440113@users.noreply.github.com>
2026-04-13 02:09:26 +0200

    Fixed contract creation bug by making client FK nullable, added another migration and updated database

--- PROG7311_GLMSApp/Models/Contract.cs
+++ PROG7311_GLMSApp/Models/Contract.cs
@@ -9,13 +9,13 @@ namespace PROG7311_GLMSApp.Models
         public DateOnly StartDate { get; set; }
         public DateOnly EndDate { get; set; }
 
-        public string Status { get; set; }
+      public string Status { get; set; }
 
-        public string ServiceLevel { get; set; }
+       public string ServiceLevel { get; set; }
 
         [ForeignKey("ClientId")] 
-      public int ClientId { get; set; }
-        public Client Client { get; set; }
+         public int ClientId { get; set; }
+        public Client? Client { get; set; }
 
        
     }
