b523b3b6f448710c3ddebd3c6436d129670576cd
diya singh <161337050+ST10440113@users.noreply.github.com>
2026-04-22 00:55:58 +0200

    altered unit tests

--- LGMS_Test/UnitTest1.cs
+++ LGMS_Test/UnitTest1.cs
@@ -16,7 +16,7 @@ namespace LGMS_Test
         public void Test1_USD_to_ZAR_Conversion()
         {
            //Arrange
-           CurrencyService currencyService = new CurrencyService(new HttpClient());
+           CurrencyService currencyService = new CurrencyService(null);
               double amount = 100;
               double exchangeRate = 16.36;
               double expected = 1636;
@@ -30,18 +30,35 @@ namespace LGMS_Test
         [Fact]
         public void Test2_FileTypeValidation()
         {
-            string fileName = "Essay.pdf";
-            string expected = ".pdf";
-            string actual = System.IO.Path.GetExtension(fileName);
-            Assert.Equal(expected, actual);
+            //Arrange
+            ContractService contractService = new ContractService(null,null,null);          
+            IFormFile file = new FormFile(null, 0, 0, null, "agreement.exe"); 
+            bool isFileValid = false;
+
+            //Act
+            try
+            {
+                contractService.CheckFileExtension(file);
+            }
+            catch (ArgumentException ex)
+            {
+                isFileValid = ex.Message.Contains("Invalid file type. Only PDF files are allowed.");
+            }
+
+            //Assert
+            Assert.True(isFileValid);
         }
 
 
         [Fact]
         public void Test3_PreventServiceRequestCreateForExpiredContracts()
         {
-            string contractStatus = "Expired";
-            bool canCreateServiceRequest = contractStatus != "Expired";
+            // Arrange
+            ContractContext contractContext = new ContractContext();
+            // Act
+            contractContext.SetState(new ConcreteContract.Expired());
+            bool canCreateServiceRequest = contractContext.ChangeState("Expired");
+            // Assert
             Assert.False(canCreateServiceRequest);
         }
     }
